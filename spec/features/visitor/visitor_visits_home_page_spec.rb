8b37c5af63883bee563b37e54dfcd03332aec252
randy-darsh <rspringer47@gmail.com>
2018-01-03 11:17:33 -0700

    first test passing

---
+++ spec/features/visitor/visitor_visits_home_page_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+describe 'Visitor visits the home page' do
+  it 'they see the home page' do
+    visit root_path
+
+    expect(page).to have_content("Squatch Watch")
+  end
+end
