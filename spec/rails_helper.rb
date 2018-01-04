bd74dda206b349ba88e1ceceec6bf11d23c741f0
randy-darsh <rspringer47@gmail.com>
2018-01-04 14:10:55 -0700

    stub omniauth method

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -36,3 +36,24 @@ RSpec.configure do |config|
 
   config.filter_rails_from_backtrace!
 end
+
+def stub_omniauth
+  # first, set OmniAuth to run in test mode
+  OmniAuth.config.test_mode = true
+  # then, provide a set of fake oauth data that
+  # omniauth will use when a user tries to authenticate:
+  OmniAuth.config.mock_auth[:google] = OmniAuth::AuthHash.new({
+    provider: "google",
+    uid: "12345678910",
+    info: {
+      email: "rspringer47@gmail.com",
+      first_name: "Randy",
+      last_name: "Springer"
+    },
+    credentials: {
+      token: "abcdefg12345",
+      refresh_token: "12345abcdefg",
+      expires_at: DateTime.now,
+    }
+  })
+end
