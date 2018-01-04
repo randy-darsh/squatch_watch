b83c18c28be3a059e3b261236259c5ceffcd7ea0
randy-darsh <rspringer47@gmail.com>
2018-01-04 14:12:19 -0700

    oauth test passing

---
+++ spec/features/user/user_logs_in_with_google_oauth_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe 'User visits home page' do
+  it 'they log in using google oauth' do
+    stub_omniauth
+
+    visit root_path
+
+    expect(page).to have_link("Login with Google")
+
+    click_link "Login with Google"
+    expect(page).to have_content("Jesse Spevack")
+    expect(page).to have_link("Logout")
+  end
+end
