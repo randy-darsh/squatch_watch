7aa20a0cd98bfd890b6c91f17cd3a6c8b3928156
randy-darsh <rspringer47@gmail.com>
2018-01-09 14:44:24 -0700

    big foot reports

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180104204954) do
+ActiveRecord::Schema.define(version: 20180109200951) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "big_foot_reports", force: :cascade do |t|
+    t.integer "report_number"
+    t.string "report_class"
+    t.string "county"
+    t.string "season"
+    t.text "location_details"
+    t.text "other_witnesses"
+    t.string "year"
+    t.string "state"
+    t.text "observed"
+    t.string "time_and_conditions"
+    t.text "also_noticed"
+    t.text "other_stories"
+    t.string "nearest_town"
+    t.string "nearest_road"
+    t.string "environment"
+    t.string "month"
+    t.string "date"
+    t.text "a_g_references"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "provider"
