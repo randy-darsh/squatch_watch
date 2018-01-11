8a7fbee2b81312ed9c50f0b1b03a28365dbc1202
randy-darsh <rspringer47@gmail.com>
2018-01-10 19:11:40 -0700

    routing error in test

--- spec/features/visitor/visitor_visits_squatch_reports_index_spec.rb
+++ spec/features/visitor/visitor_visits_squatch_reports_index_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe 'Visitor visits the squatch reports page' do
   it 'they see a list of the squatch reports' do
-    visit squatch_reports_path
+    visit big_foot_reports_path
 
     expect(page).to have_content("Squatch Reports")
   end
