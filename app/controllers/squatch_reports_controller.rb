556718acac5667553604fe9b8b06fca5550bda2a
randy-darsh <rspringer47@gmail.com>
2018-01-10 19:07:02 -0700

    changing all instances of squatch report to big foot report

--- app/controllers/squatch_reports_controller.rb
+++
@@ -1,9 +0,0 @@
-class SquatchReportsController < ApplicationController
-  def index
-    @squatch_reports = BigFootReport.order_by_report_number.paginate(:page => params[:page], :per_page => 30)
-  end
-
-  def show
-    @squatch_report = BigFootReport.find(params[:id])
-  end
-end
