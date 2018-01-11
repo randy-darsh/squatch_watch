d2ae7b91280cba3f14867bc76a57dc82bb7fe8c6
randy-darsh <rspringer47@gmail.com>
2018-01-11 09:46:46 -0700

    report factory

---
+++ spec/factories/big_foot_reports.rb
@@ -0,0 +1,21 @@
+FactoryBot.define do
+  factory :big_foot_report do
+    report_class "class"
+    county "county"
+    season "season"
+    location_details "location details"
+    other_witnesses "other witnesses"
+    year "year"
+    state "state"
+    observed "observed"
+    time_and_conditions "time and conditions"
+    also_noticed "also_noticed"
+    other_stories "other stories"
+    nearest_town "nearest town"
+    nearest_road "nearest road"
+    environment "environment"
+    month "month"
+    date "day"
+    a_g_references "ag references"
+  end
+end
