bf3d2b33f3a521968b8b4b2e9ce33905c411e268
randy-darsh <rspringer47@gmail.com>
2018-01-10 23:55:10 -0700

    make it so only logged in users can use the search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,9 +1,14 @@
 class SearchController < ApplicationController
   def index
-    @search_by_state = BigFootReport.search_by_state(params[:search]).paginate(:page => params[:page], :per_page => 30)
-    @search_by_report_number = BigFootReport.search_by_report_number(params[:search])
-    @search_by_year = BigFootReport.search_by_year(params[:search]).paginate(:page => params[:page], :per_page => 30)
-    @search_by_season = BigFootReport.search_by_season(params[:search]).paginate(:page => params[:page], :per_page => 30)
-    @search_by_report_class = BigFootReport.search_by_report_class(params[:search]).paginate(:page => params[:page], :per_page => 30)
+    if current_user
+      @search_by_state = BigFootReport.search_by_state(params[:search]).paginate(:page => params[:page], :per_page => 30)
+      @search_by_report_number = BigFootReport.search_by_report_number(params[:search])
+      @search_by_year = BigFootReport.search_by_year(params[:search]).paginate(:page => params[:page], :per_page => 30)
+      @search_by_season = BigFootReport.search_by_season(params[:search]).paginate(:page => params[:page], :per_page => 30)
+      @search_by_report_class = BigFootReport.search_by_report_class(params[:search]).paginate(:page => params[:page], :per_page => 30)
+    else
+      redirect_to root_path
+      flash[:notice] = "Please create an account or login first"
+    end
   end
 end
