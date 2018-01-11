ad7aa54c7793e85634033daaa296d87d223b70e4
randy-darsh <rspringer47@gmail.com>
2018-01-10 21:46:53 -0700

    removed filter for current user for maps

--- app/controllers/squatch_map_controller.rb
+++ app/controllers/squatch_map_controller.rb
@@ -1,10 +1,5 @@
 class SquatchMapController < ApplicationController
   def index
-    if current_user
-      @big_foot_reports = BigFootReport.all
-    else
-      redirect_to root_path
-      flash[:notice] = "Please create an account or login first"
-    end
+    @big_foot_reports = BigFootReport.all
   end
 end
