29a0a4f894de265a7c2aac4f686f691015d4ebac
randy-darsh <rspringer47@gmail.com>
2018-01-11 09:48:05 -0700

    log out test

--- spec/features/user/user_logs_in_with_google_oauth_spec.rb
+++ spec/features/user/user_logs_in_with_google_oauth_spec.rb
@@ -12,4 +12,19 @@ describe 'User visits home page' do
     expect(page).to have_content("Randy Springer")
     expect(page).to have_link("Logout")
   end
+
+  it 'they can log out' do
+    stub_omniauth
+
+    visit root_path
+
+    expect(page).to have_link("Login with Google")
+
+    click_link "Login with Google"
+    expect(page).to have_content("Randy Springer")
+    expect(page).to have_link("Logout")
+
+    click_link("Logout")
+    expect(page).to have_link("Login with Google")
+  end
 end
