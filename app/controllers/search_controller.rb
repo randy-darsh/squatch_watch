341e07015e64e1a88435ba5cceb0631fbb05cb6a
randy-darsh <rspringer47@gmail.com>
2018-01-10 23:29:00 -0700

    more active record search methods

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,5 +2,8 @@ class SearchController < ApplicationController
   def index
     @search_by_state = BigFootReport.search_by_state(params[:search]).paginate(:page => params[:page], :per_page => 30)
     @search_by_report_number = BigFootReport.search_by_report_number(params[:search])
+    @search_by_year = BigFootReport.search_by_year(params[:search]).paginate(:page => params[:page], :per_page => 30)
+    @search_by_season = BigFootReport.search_by_season(params[:search]).paginate(:page => params[:page], :per_page => 30)
+    @search_by_report_class = BigFootReport.search_by_report_class(params[:search]).paginate(:page => params[:page], :per_page => 30)
   end
 end
