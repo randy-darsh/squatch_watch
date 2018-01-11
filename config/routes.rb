959fadc0b2656ebee581a3b0361f2e7bf8b81317
randy-darsh <rspringer47@gmail.com>
2018-01-10 22:53:16 -0700

    add search route and troubleshooting routing error

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   root 'welcome#index'
 
+  resources :big_foot_reports, only: [:index, :show, :new, :create]
+
   get '/about',                   to: 'about#index'
   get 'auth/:provider/callback',  to: 'sessions#create'
   get 'logout',                   to: 'sessions#destroy'
   get '/squatch_map',             to: 'squatch_map#index'
-
-  resources :big_foot_reports, only: [:index, :show, :new, :create]
+  get '/search',                  to: 'search#index'
 end
