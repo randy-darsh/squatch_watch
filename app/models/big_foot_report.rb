f246cbafee991fc3b564d7990d3db01bb3a83ba2
randy-darsh <rspringer47@gmail.com>
2018-01-10 19:05:10 -0700

    find date method

--- app/models/big_foot_report.rb
+++ app/models/big_foot_report.rb
@@ -1,11 +1,14 @@
 class BigFootReport < ApplicationRecord
   validates :report_number, presence: :true
-
   def self.state_count(state_name)
     where(state: state_name).count
   end
 
   def self.order_by_report_number
     order(:report_number)
+  end
+
+  def find_date
+    "#{date} #{month} #{year}"
   end
 end
